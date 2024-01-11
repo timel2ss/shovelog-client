ec759923ccd605f37b69e26399fa0d38b85f35b2
timel2ss <dyddn0823@hanyang.ac.kr>
2024-01-11 22:58:40 +0900

    feat: Header Component 구현

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,11 @@
+import { Logo, Menu, Search } from "@/components/vectors";
+
+export default function Header() {
+  return (
+    <header className={"h-14 border-b-gray2 border-b px-4 flex items-center justify-center"}>
+      <Menu className={"h-8 w-8 text-gray0 absolute left-4 flex items-center"}/>
+      <Logo className={"w-24 text-gray0"}/>
+      <Search className={"h-8 w-8 text-gray0 absolute right-4 flex items-center"}/>
+    </header>
+  )
+}
