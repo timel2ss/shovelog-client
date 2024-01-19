2b2a616f36cd4ec8e9246ff06ecfd4b18b7282d2
timel2ss <dyddn0823@hanyang.ac.kr>
2024-01-19 23:05:20 +0900

    feat: Category fetch API

---
+++ src/api/category.ts
@@ -0,0 +1,15 @@
+export type category = {
+  id: number,
+  name: string,
+  description: string
+}
+
+export async function fetchCategories() {
+  try {
+    const url: string = `${process.env.NEXT_PUBLIC_API_URL}/api/v1/categories`
+    const response: Response = await fetch(url)
+    return await response.json()
+  } catch (e) {
+    console.log('Error occurs on fetching API', e)
+  }
+}
